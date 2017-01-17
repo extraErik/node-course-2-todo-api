35cccd4cf2723199449dfb53f50d93abf31c114e
extraErik <erygg01@live.com>
2017-01-17 13:15:38 -0600

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,6 +46,34 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    // get the id
+
+    //validate the id -> if not valid return 404
+
+    // remove todo by id
+        // success
+            // if no doc send 404
+            // if doc send doc back with 200
+        // error
+            // 400 with empty body
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 
 });
 
