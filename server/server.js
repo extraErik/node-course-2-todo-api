0a8757545a0d5e24d945b8a6d833fead39d793bf
extraErik <erygg01@live.com>
2017-01-18 21:25:38 -0600

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -96,7 +96,6 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
-// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -125,6 +124,14 @@ app.post('/users/login', (req, res) => {
         res.status(400).send();
     });
 });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send()
+    }, () => {
+        res.status(400).send();
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
